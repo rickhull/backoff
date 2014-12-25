db59e1417868ae5cabcbaea79a27a0fc4cf0a0fb
Rick Hull <rick.hull@gmail.com>
2014-12-25 00:42:21 -0500

    rework

--- lib/backoff.ex
+++ lib/backoff.ex
@@ -1,31 +1,56 @@
 defmodule Backoff do
+  # write a function f that returns true or false
+  # pass a function g like &exp(&1, 5) or &poly/1
   def until(f, g, n \\ 0) when is_function(f) and is_function(g) do
     val = f.()
     if val do
       val
     else
-      g.(n) |> :timer.sleep
+      (g.(n) * 100) |> round |> :timer.sleep
       until(f, g, n + 1)
     end
   end
-  # 2^n
-  def exp(n), do: :math.pow(2, n)
-  # n^2
-  def poly(n), do: :math.pow(n, 2)
+
+  def exp(n, base \\ 2), do: :math.pow(base, n)  # 2^n
+  def poly(n, exp \\ 2), do: :math.pow(n, exp)   # n^2
+  def lin(n), do: n
 
   def fib(n) when is_integer(n) and n >= 2, do: fib(n-2) + fib(n-1)
   def fib(n) when is_integer(n) and n >= 0, do: n
+end
 
-  #def timestamp_ms do
-  #  now = :erlang.now
-  #  {_, {h, m, s}} = :calendar.now_to_local_time(now)
-  #  "#{h}:#{m}:#{s}.#{elem(now, 2)}"
-  #end
+defmodule Example do
+  def foo(bar, baz) when is_integer(bar) and is_integer(baz) do
+    IO.puts "Attempting a failure-prone remote operation..."; :timer.sleep 500
+    if :random.uniform(bar) == baz do
+      IO.puts "SUCCESS"
+      {:ok, bar * baz}
+    else
+      IO.puts "FAILED"
+      {:error, "System Unavailable"}
+    end
+  end
 
+  # exponential backoff of foo/2
+  def demo do
+    Backoff.until(fn ->
+                    case Example.foo(5, 2) do
+                      {:ok, _val} -> true
+                      {:error, _msg} -> false
+                      _ -> raise("broken")
+                    end
+                  end,
+                  &Backoff.exp/1)
+
+  end
+end
+
+# used for a dumb benchmark thing
+defmodule FibTimer do
   def main(args) do
     start = :erlang.now
     {n,_} = args |> hd |> Integer.parse
-    IO.puts fib(n)
+    IO.puts "fib(#{n}) #{Backoff.fib(n)}"
 
     secs = elapsed_secs(start, :erlang.now)
     IO.puts "self\t#{div(secs, 60)}m#{rem(secs, 60)}s"
